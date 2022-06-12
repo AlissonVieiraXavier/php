cfd0998030674184b545ac5e92585f4c6d8913df
Alisson Xavier <78052229+AlissonVieiraXavier@users.noreply.github.com>
2022-06-12 12:23:36 -0300

    refatoração de codigo

--- D.Register/routes/web.php
+++ D.Register/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\GeralController;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/painel',[GeralController::class,'index']);
+Route::get('/',[AuthController::class,'index']);
+Route::get('/painel',[MainController::class,'index']);
