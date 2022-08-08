f833473a27d81926ce944158a54d743ffd62bf3e
AlissonVieiraXavier <alissonvieiraxavier@hotmail.com>
2022-08-08 20:01:26 -0300

    migrations

---
+++ Laravel/CrudLaravel/database/migrations/2022_08_08_224200_create_produtos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');//esse campo espera um texto
+            $table->decimal('custo',19,2);//este campo espera um tipo decimal
+            $table->integer('quantidade');//esse campo espera um tipo inteiro
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
---
+++ Laravel/CrudLaravel/resources/views/produtos/create.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar um novo Produto</title>
+</head>
+<body>
+   <form action="">
+    <label for="">Nome</label>
+    inpu
+   </form>   
+
+
+   <!-- Vamos começar criando uma página com um formulário simples, como o nosso foco nesse tutorial é o Laravel utilizaremos os recursos mais básicos do html, apenas para conseguirmos preencher e enviar um formulário, ok?
+
+O Laravel por padrão armazena e reconhece seus arquivos de páginas html em resources/views, localize esse diretório e crie uma pasta chamada produtos, em seguida, dentro dessa nova pasta, crie um arquivo chamado create.blade.php.
+
+Edite o seu novo arquivo, create.blade.php, para que fique desta forma:
+
+-->
+</body>
+</html>
