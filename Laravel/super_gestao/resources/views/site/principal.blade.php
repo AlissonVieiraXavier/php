69abb96bae52425676de371caf583b83ef8aebc6
AlissonVieiraXavier <alissonvieiraxavier@hotmail.com>
2022-10-01 20:39:43 -0300

    ds

--- Laravel/super_gestao/resources/views/site/principal.blade.php
+++ Laravel/super_gestao/resources/views/site/principal.blade.php
@@ -1,1 +1,10 @@
-<h1>nheee2</h1>
+<h1>principal</h1>
+
+<ul>
+    <li><a href="/">Principal</a></li>
+    <li><a href="{{ route('site.sobre-nos') }}">Sobre</a></li>
+    <li><a href="/contato">Contato</a></li>
+    <li><a href="/login">Login</a></li>
+    <li><a href="/fornecedores">Fornecedores</a></li>
+    <li><a href="/produtos">Contato</a></li>
+</ul>
--- Laravel/super_gestao/routes/web.php
+++ Laravel/super_gestao/routes/web.php
@@ -5,11 +5,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\ContatoController;
 
+//rota teste nome;
+//Route::get('/contato/gg',function(){ return 'ohio';})->name;
+//
 
-Route::get('/', [PrincipalController::class, 'index']);
-Route::get('/sobre-nos', [SobreNosController::class, 'index']);
-Route::get('/contato',[ContatoController::class,'index']);
-Route::get('/contato/{name}', function (string $name) {
-      return "hello there!  ".$name;                          
+Route::get('/', [PrincipalController::class, 'index'])->name('site.index');
+Route::get('/sobre-nos', [SobreNosController::class, 'index'])->name('site.sobre-nos');
+Route::get('/contato',[ContatoController::class,'index'])->name('site.contato');
+Route::get('/login',function (){ return 'login';})->name('site.login');
+
+Route::prefix('/app')->group(function(){
+      Route::get('/clientes',function (){ return 'clientes';});
+      Route::get('/fornecedores',function (){ return 'fornecedores';});
+      Route::get('/produtos',function (){ return 'produtos';});
 });
+
 
